a8d40cb72336422ebd0e4014f76b3ad2db78662f
津端 俊尚 <t-tsubata@i-enter.co.jp>
2022-05-13 09:39:50 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 津端 俊尚 on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
